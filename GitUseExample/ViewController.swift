0e216df057d2fd8e49a2c8fc4471fb2865796c44
Alrik Künne <alrik.kuenne@gmail.com>
2020-03-11 22:36:30 +0100

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Alrik Künne on 11.03.20.
+//  Copyright © 2020 Alrik Künne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

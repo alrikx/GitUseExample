e0d17049706c0e1cb20186ff7d1cb24829cd82f9
Alrik Künne <alrik.kuenne@gmail.com>
2020-03-11 23:01:14 +0100

    test a feature branch

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,15 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Alrik Künne on 11.03.20.
+//  Copyright © 2020 Alrik Künne. All rights reserved.
+//
+
+import Foundation
+
+class MapForItinerary: NSObject {
+    func sayHello(to name:String){
+        print("Hello" + name)
+    }
+}
